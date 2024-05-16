d31d1cec685fba0d30483c82991af000b2080575
dirkarnez <smalldirkalex@gmail.com>
2024-05-17 02:39:56 +0800

    - working

--- main/main.ino
+++ main/main.ino
@@ -1,90 +1,34 @@
-//#include <gsm_a6.h>
-//
-//
-//
-//#define RX 7
-//#define TX 8
-//#define RESET 2
-//#define BAUD 9600
-//
-//
-//GSMSim gsm;
-//
-///*
-// * Also you can this types:
-// * GSMSim gsm(RX, TX);
-// * GSMSim gsm(RX, TX, RESET);
-// * GSMSim gsm(RX, TX, RESET, LED_PIN, LED_FLAG);
-// */
-//
-//void setup() {
-//  // put your setup code here, to run once:
-//  
-//  Serial.begin(9600);
-//
-//  Serial.println("GSMSim Library - Call Example");
-//  Serial.println("");
-//  delay(1000);
-//
-//  gsm.start(); // baud default 9600
-//  //gsm.start(BAUD);
-//
-//  char* phone_no = "xxxxxxxx";
-//
-//  Serial.println("Calling the number " + String(phone_no));
-//  Serial.println(gsm.call(phone_no));
-//  delay(100000);
-//  Serial.println("Call hang off");
-//  gsm.callHangoff();
-//
-//  
-//
-//  
-//}
-//
-//void loop() {
-//  // put your main code here, to run repeatedly:
-//}
+#include <at24c08.h>
 
-#include <SoftwareSerial.h>
+// Create a eprom object configured at address 0
+// Sketch assumes that there is an eprom present at this address
+AT24C08 eprom(AT24C08_ADDRESS_0);
 
-//Create software serial object to communicate with A6
-SoftwareSerial mySerial(2, 4); //A6 Tx & Rx is connected to Arduino #2 & #4
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting up");
 
-void setup()
-{
-//Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
-Serial.begin(115200);
-
-//Begin serial communication with Arduino and A6
-mySerial.begin(115200);
-
-Serial.println("Initializing...");
-delay(1000);
-
-mySerial.println("AT"); //Once the handshake test is successful, i t will back to OK
-updateSerial();
-
-mySerial.println("ATD+852xxxxxxxx"); // change ZZ with country code and xxxxxxxxxxx with phone number to dial
-updateSerial();
-delay(20000); // wait for 20 seconds...
-mySerial.println("ATH"); //hang up
-updateSerial();
+  // Initialize the i2c library
+  Wire.begin();
+  int foo_in;
+  eprom.get(0, foo_in);
+  Serial.println(foo_in);
+  
+  /** Write and read an integer */
+  int foo = 42;
+  // Write the integer foo to the eprom starting at address 0
+  eprom.put(0, foo);
+  eprom.get(0, foo_in);
+  // Read the integer foo_in from eprom starting at address 0
+  eprom.get(0, foo_in);
+  Serial.println(foo_in);
+  
+  // Get the last error code, it should be 0 since there was no error
+  int lastError = eprom.getLastError();
+  Serial.print("last status on write: ");
+  Serial.println(lastError);
 }
 
-void loop()
-{
-}
-
-void updateSerial()
-{
-delay(500);
-while (Serial.available())
-{
-mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
-}
-while(mySerial.available())
-{
-Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
-}
+// This test program has no loop, it just runs once
+void loop() {
 }
